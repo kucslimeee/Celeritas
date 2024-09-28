876abf5c434cef2abf8dd3fcceaecf1d4a4860f1
HadAndras <147320709+HadAndras@users.noreply.github.com>
2024-09-28 21:04:04 +0200

    Update i2c_queue.c

--- Core/Src/i2c_queue.c
+++ Core/Src/i2c_queue.c
@@ -63,3 +63,74 @@
  	queue.tail = 0;
  	queue.size = 0;
  }
+
+
+void add_header(Request request, uint16_t duration){
+	 uint8_t headerData[ITEM_SIZE];
+	 uint16_t localDur = duration;
+	 uint32_t localTime = request.start_time;
+	 headerData[0] = request.ID;
+	 //headearData[1] = temperature;
+	 headerData[2] = (uint8_t)(localDur >> 8);
+	 localDur -= (headerData[2] << 8);
+	 headerData[3] = (uint8_t)(localDur);
+	 for(int i = 0; i < 4; i++){
+		 headerData[4+i] = (uint8_t)(localTime >> 24-i*8);
+		 localTime -= (headerData[i] << 24-i*8);
+	 }
+	 //headerData[9] = request.number_of_Intervals;
+	 headerData[10] = request.min_voltage;
+	 headerData[11] = request.max_voltage;
+	 headerData[12] = 0;
+	 headerData[13] = 0;
+	 headerData[14] = 0;
+	 headerData[15] = 0xFF;
+	 //queue_push(headerData, priority???, checksum???);
+
+ }
+
+
+ void add_spectrum(Request request, unsigned int* spectrum, uint8_t resolution){
+	 const uint8_t everyBit = 15*8; //120
+	 uint8_t importantBits = everyBit/resolution;
+	 uint8_t bitArr[everyBit]; //120 byte
+	 uint8_t data[ITEM_SIZE-1];
+	 for(int i = 0; i < resolution; i++){
+		 for(int j = 0; j < importantBits; j++){
+			 if(spectrum[i] % 2 == 0){
+				 spectrum[i] /= 2;
+				 bitArr[(i+1)*importantBits-j-1] = 0;
+			 }
+			 else{
+				 spectrum[i] = (spectrum[i]-1)/2;
+				 bitArr[(i+1)*importantBits-j-1] = 1;
+			 }
+		 }
+	 }
+
+	 for(int k = 0; k < ITEM_SIZE-1; k++){
+		 for(int m = 0; m < 8; m++){
+			 if(bitArr[k*8+m] == 1){
+				 data[k] += pow(2, 7-m);
+			 }
+		 }
+	 }
+	 //queue_push(data, priority???, checksum???);
+
+
+
+}
+
+ void add_error(Request request, uint8_t error_type){
+	 uint8_t errorData[15] = {0};
+	 if(error_type == TIMEOUT){
+		 errorData[14] = 0xFD;
+	 }
+	 if(error_type == INTERRUPT){
+		 errorData[14] = 0xFB;
+	 }
+	 if(error_type == CORRUPTED){
+	 	errorData[14] = 0xF7;
+	 }
+	 //queue_push(errorData, priority???, checksum???);
+ }
