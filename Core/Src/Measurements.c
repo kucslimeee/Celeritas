af00eafda80feff3c97fa924ee608d25e8c4fe13
HadAndras <147320709+HadAndras@users.noreply.github.com>
2024-09-28 21:00:56 +0200

    Update Measurements.c

--- Core/Src/Measurements.c
+++ Core/Src/Measurements.c
@@ -6,32 +6,72 @@
  */
 #include"main.h"
 #include"exp_i2c_slave.h"
+#include"Request.h"
+#include<i2c_queue.h>
+#include<Checksum.h>
 
 
-const uint8_t triggerVoltage = 200;
+
 #define TxSIZE 16
-extern uint8_t TxData[TxSIZE];
+
+//extern uint8_t TxData[TxSIZE];
 extern ADC_HandleTypeDef hadc1;
-extern TIM_HandleTypeDef htim1;
-int ready = 1;
-uint8_t voltage = 0;
-int deltaTime = 0;
+//extern TIM_HandleTypeDef htim1;
 
 
-void measure()
-{
-	int readValue = 0;
 
-	__HAL_TIM_SET_COUNTER(&htim1, 0);  // set the counter value a 0
-	readValue = analogRead();
-	voltage = (uint8_t)(readValue*255/4095);
-	if(voltage > triggerVoltage){
-		push((uint8_t)voltage);
-		//deltaTime = __HAL_TIM_GET_COUNTER(&htim1);
-		//push(deltaTime);
+void max_hit_measurement(Request request, uint8_t resolution, uint8_t duration, uint8_t samples){
+	uint8_t measurementData[resolution]; //max resolution 15
+	uint8_t internalLength = (request.max_voltage - request.min_voltage)/resolution;
+	uint8_t peaks = 0;
+	uint8_t intervalIndex = 0;
+	while(peaks < duration){
+		intervalIndex = (uint8_t)((samples_adc(samples, request.min_voltage, request.max_voltage)
+				-request.min_voltage)/internalLength);
 
+		measurementData[intervalIndex]++;
+		peaks++;
 	}
 
+	if(request.is_header){
+		if(request.is_priority){
+			//send data
+			//send header
+		}
+		else{
+			//send header
+			//send data
+		}
+	}
+	else{
+		//send data with a given priority
+	}
+
+}
+
+uint8_t sample_adc(uint8_t samples, uint8_t min_voltage, uint8_t max_voltage){
+	uint8_t count = 0;
+	uint8_t readVal;
+	uint8_t lastSample;
+	uint8_t SamplesData[samples];
+	int sum = 0;
+
+	while(count  < samples){
+		readVal = (uint8_t)(analogRead()*255/4095); //measure ADC
+		if(readVal > min_voltage && readVal < max_voltage){
+			if(readVal > lastSample){
+				SamplesData[count] = readVal;
+				count++;
+			}
+			lastSample = readVal;
+			sum += readVal;
+		}
+
+	}
+	HAL_GPIO_WritePin(GPIOA, GPIO_Pin_6, GPIO_PIN_SET);
+	HAL_GPIO_Delay(10);
+	HAL_GPIO_WritePin(GPIOA, GPIO_Pin_6; GPIO_PIN_RESET);
+	return (uint8_t)(sum/count);
 }
 
 int analogRead()
