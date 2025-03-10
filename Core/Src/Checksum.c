967fa0315ce77e46356eb9c746b997a7105d3c4b
kucslimeee <roland.gerendas@gmail.com>
2025-03-10 20:42:31 +0100

    Checksum update
    new method: adding the decimal places having ones (refer to command gen)

--- Core/Src/Checksum.c
+++ Core/Src/Checksum.c
@@ -7,7 +7,7 @@
 
  #include "Checksum.h"
 
- uint8_t calculate_checksum(uint8_t item[], int length) {
+ /*uint8_t calculate_checksum(uint8_t item[], int length) { //old checksum
      uint8_t checksum = 0;
      uint16_t sum = 0;
      for (int i = 0; i < length; i++) {
@@ -15,4 +15,30 @@
      }
      checksum = (uint8_t)sum;
      return checksum;
- }
+ }*/
+
+int power_calc(int base, int exponent){         //power function / hatvány függvény
+    int value = 1;
+    for (int i = 0; i < exponent; i++){
+        value = value * base;
+    }
+    return value;
+}
+
+uint8_t calculate_checksum(uint8_t item[], int length){                      // new checksum
+    uint8_t sum = 0;                                // return value
+    uint8_t count[8];                               // for storing seperately
+
+    for (int i = 0; i < length; i++) {                  // go through each byte
+
+        count[i] = item[i];
+
+        for (int k = 7; k >= 0; k--){               // count the number of decimal places with ones in binary
+            if (count[i] >= power_calc(2, k)){
+                count[i] = count[i] - power_calc(2, k);
+                sum++;                              // add them up
+            }
+        }
+    }
+    return sum;                            // 7*8 = 56 is the maximum checksum value, minimum is 0
+}
